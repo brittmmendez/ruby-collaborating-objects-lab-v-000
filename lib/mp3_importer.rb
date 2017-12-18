b439cea35c331dd96051c785f5b8964f0fe2a67c
Brittany Morris <brittmmendez@gmail.com>
2017-12-18 14:39:12 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ def initialize(path)
   @path=path
 end
 
-def file
+def files
   #this link helps explain this: https://stackoverflow.com/questions/46575228/how-to-normalise-the-filename-to-just-the-mp3-filename-with-no-path-ruby
    Dir.chdir(@path) {| path | Dir.glob("*.mp3")}
 end
